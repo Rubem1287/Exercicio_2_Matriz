b9a266a0cd361bc647a6ae6749e5de6e4ca6718a
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:57:21 -0300

    Add files via upload

---
+++ Exercício 2 M/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercício_2_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a;
+
+            int n = int.Parse(Console.ReadLine());
+
+            a = new int[n, n];
+
+            int soma = 0;
+
+            for (int l = 0; l < n; l++)
+            {
+                string[] vetor = Console.ReadLine().Split(' ');
+                for (int c = 0; c < n; c++)
+                {
+                    a[l, c] = int.Parse(vetor[c]);
+                }
+            }
+
+            Console.WriteLine();
+            for (int l = 0; l < n; l++)
+            {
+
+                for (int c = 0; c < n; c++)
+                {
+                    soma = soma + a[l, c];
+                }
+
+                Console.WriteLine(soma);
+                soma = 0;
+
+            }
+        }
+    }
+}
